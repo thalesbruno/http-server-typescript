e4a9d7cdd948cb5b0871760118a8bbbe85ff6b41
Thales <thalesbrno@gmail.com>
2025-03-22 15:30:54 -0300

    conditional response

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,28 @@
 import * as net from "net";
 
+const CRLF = '\r\n';
+
+const extractRequestLine = (httpRequest: string) => {
+  const [requestLine, ..._headersAndBody] = httpRequest.split(CRLF);
+  return requestLine;
+}
+
 const server = net.createServer((socket) => {
-  socket.on("connect", () => {
-    console.log("connected");
-  });
   socket.on("close", () => {
     socket.end();
   });
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const httpRequest = data.toString();
+    const requestLine = extractRequestLine(httpRequest);
+    const [_method, path, _version] = requestLine.split(' ');
+    console.log(requestLine);
+
+    if (path === '/') {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
   });
 });
 
