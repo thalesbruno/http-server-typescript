3f0a6e3d1a21445569aceaa180eb53d2f0fd5e1d
Thales <thalesbrno@gmail.com>
2025-03-22 16:12:00 -0300

    responde with body

--- app/main.ts
+++ app/main.ts
@@ -1,28 +1,54 @@
 import * as net from "net";
 
-const CRLF = '\r\n';
+const CRLF = "\r\n";
+const DOUBLE_CRLF = "\r\n\r\n";
 
-const extractRequestLine = (httpRequest: string) => {
-  const [requestLine, ..._headersAndBody] = httpRequest.split(CRLF);
-  return requestLine;
-}
+const parseRequest = (httpRequest: string) => {
+  const [_requestLine, ...headersAndBody] = httpRequest.split(CRLF);
+  const [method, path, version] = _requestLine.split(" ");
+  const requestLine = { method, path, version };
+  const [headers, body] = headersAndBody.join("").split(DOUBLE_CRLF);
+  return { requestLine, headers, body };
+};
+
+const StatusLineReason = {
+  200: "OK",
+  404: "Not Found",
+} as const;
+type StatusCode = keyof typeof StatusLineReason;
+
+const buildResponse = (statusCode: StatusCode, body: string): string => {
+  const statusLine = `HTTP/1.1 ${statusCode} ${StatusLineReason[statusCode]}`;
+
+  const headers = new Map<string, string>();
+  headers.set("Content-Type", "text/plain");
+  headers.set("Content-Length", body.length.toString());
+  const headersString = Array.from(headers.entries())
+    .map(([key, value]) => `${key}: ${value}`)
+    .join(CRLF);
+
+  return `${statusLine}${CRLF}${headersString}${DOUBLE_CRLF}${body}`;
+};
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
   socket.on("data", (data) => {
-    const httpRequest = data.toString();
-    const requestLine = extractRequestLine(httpRequest);
-    const [_method, path, _version] = requestLine.split(' ');
-    console.log(requestLine);
+    const {
+      requestLine: { path },
+    } = parseRequest(data.toString());
 
-    if (path === '/') {
+    if (path.startsWith("/echo/")) {
+      const [_, str] = path.split("/echo/");
+      const response = buildResponse(200, str);
+      socket.write(response);
+    }
+    if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
-
   });
 });
 
