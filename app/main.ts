90869c5934c5d7ab6402beb74bd6a2ee8c0ca716
Thales <thalesbrno@gmail.com>
2025-03-22 17:05:45 -0300

    respond with user agent

--- app/main.ts
+++ app/main.ts
@@ -4,16 +4,25 @@ const CRLF = "\r\n";
 const DOUBLE_CRLF = "\r\n\r\n";
 
 const parseRequest = (httpRequest: string) => {
-  const [_requestLine, ...headersAndBody] = httpRequest.split(CRLF);
+  const [requestLineAndHeaders, ...body] = httpRequest.split(DOUBLE_CRLF);
+  const [_requestLine, ...headers] = requestLineAndHeaders.split(CRLF);
   const [method, path, version] = _requestLine.split(" ");
   const requestLine = { method, path, version };
-  const [headers, body] = headersAndBody.join("").split(DOUBLE_CRLF);
-  return { requestLine, headers, body };
+
+  const headersMap = new Map<string, string>();
+  headers.forEach((header) => {
+    const [key, value] = header.split(": ");
+    headersMap.set(key, value);
+  });
+  const getHeader = (key: string) => headersMap.get(key);
+
+  return { requestLine, headers, getHeader, body };
 };
 
 const StatusLineReason = {
   200: "OK",
   404: "Not Found",
+  400: "Bad Request",
 } as const;
 type StatusCode = keyof typeof StatusLineReason;
 
@@ -37,18 +46,35 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const {
       requestLine: { path },
+      getHeader,
     } = parseRequest(data.toString());
+
+    if (path === "/user-agent") {
+      const userAgent = getHeader("User-Agent");
+      if (!userAgent) {
+        const response = buildResponse(400, "Bad Request");
+        socket.write(response);
+      } else {
+        const response = buildResponse(200, userAgent);
+        socket.write(response);
+      }
+      return;
+    }
 
     if (path.startsWith("/echo/")) {
       const [_, str] = path.split("/echo/");
       const response = buildResponse(200, str);
       socket.write(response);
+      return;
     }
+
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      return;
     }
+
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    return;
   });
 });
 
