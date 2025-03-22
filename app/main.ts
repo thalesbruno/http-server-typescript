629aa1291e3fcc0cfc21212db4e95900c3934a97
Thales <thalesbrno@gmail.com>
2025-03-21 22:10:14 -0300

    respond with 200

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,14 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
+  socket.on("connect", () => {
+    console.log("connected");
+  });
   socket.on("close", () => {
     socket.end();
+  });
+  socket.on("data", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
   });
 });
 
