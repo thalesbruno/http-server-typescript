4de634291045e145619bf16aff44e465e3fc816e
Thales <thalesbrno@gmail.com>
2025-03-23 19:04:37 -0300

    another try

--- app/compression.ts
+++ app/compression.ts
@@ -2,7 +2,7 @@ import { $ } from "bun";
 export const SUPPORTED_ENCODINGS = ["gzip"];
 
 export const compress = async (text: string) => {
-  const { stdout } = await $`echo ${text} | gzip`.quiet();
+  const { stdout } = await $`echo -n ${text} | gzip`.quiet();
   const size = stdout.byteLength;
 
   return { compressed: stdout, size };
