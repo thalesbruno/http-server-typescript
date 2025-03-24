4d0578b3a8538998e183ecca72500075e5ae0719
Thales <thalesbrno@gmail.com>
2025-03-23 22:23:19 -0300

    one mor try

--- app/compression.ts
+++ app/compression.ts
@@ -1,11 +1,20 @@
-import { $ } from "bun";
+import { $, gzipSync } from "bun";
 export const SUPPORTED_ENCODINGS = ["gzip"];
 
-export const compress = async (text: string) => {
-  const {stdout}= await $`echo -n "${text.trim()}" | gzip`
-  const output = new Blob([stdout]);
+export const $compress = async (text: string) => {
+  const output = await $`echo ${text} | gzip`.blob();
   return {
     compressed: output,
     size: output.size,
   };
 };
+
+export const compress = async (text: string) => {
+  const compressed = gzipSync(new TextEncoder().encode(text));
+  return {
+    compressed: new Blob([compressed]),
+    size: compressed.length,
+  };
+};
+
+
