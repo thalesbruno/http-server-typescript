428aef9ce750ee439b0326b7712e63825709cc33
Thales <thalesbrno@gmail.com>
2025-03-23 22:14:10 -0300

    try removing new line

--- app/compression.ts
+++ app/compression.ts
@@ -2,7 +2,8 @@ import { $ } from "bun";
 export const SUPPORTED_ENCODINGS = ["gzip"];
 
 export const compress = async (text: string) => {
-  const output = await $`echo -n "${text.trim()}" | gzip`.blob();
+  const {stdout}= await $`echo -n "${text.trim()}" | gzip`
+  const output = new Blob([stdout]);
   return {
     compressed: output,
     size: output.size,
